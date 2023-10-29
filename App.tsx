1ca2ce6589df5697520ce6bb18a899393afba39d
Diego-Batista <diegobtistadev@gmail.com>
2023-10-29 14:21:21 -0300

    Loading and Status Bar Component

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
+import { Loading } from '@components/Loading';
 import { NunitoSans_400Regular, NunitoSans_700Bold, useFonts } from '@expo-google-fonts/nunito-sans';
 import { HomeScreen } from '@screens/Home';
 import theme from '@theme/index';
-import { StatusBar } from 'expo-status-bar';
-import { ActivityIndicator, StyleSheet, View } from 'react-native';
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 
 export default function App() {
@@ -12,20 +12,14 @@ export default function App() {
   })
   return (
     <ThemeProvider theme={theme}>
-      <View style={styles.container}>
-        {fontsLoaded ? ( <HomeScreen /> ): ( <ActivityIndicator color='#000' size='large'/> )}
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? ( <HomeScreen /> ): ( <Loading /> )}
         
-        <StatusBar style="auto" />
-      </View>
     </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -5,10 +5,10 @@ export const Container = styled.View`
     flex: 1;
     align-items: center;
     justify-content: center;
-    background-color: ${({ theme }: any) => theme.colors.base.gray_7};
+    background-color: ${({ theme }) => theme.colors.base.gray_7};
 `
 
 export const Title = styled.Text`
-    color: ${({ theme }: any) => theme.colors.brand.red_dark };
+    color: ${({ theme }) => theme.colors.brand.red_dark };
     font-size: 50px;
 `
