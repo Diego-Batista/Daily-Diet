7156b97ba262b26887e4779baec3877bf8419783
Diego-Batista <diegobtistadev@gmail.com>
2023-11-10 16:58:47 -0300

    Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react"
+import { TouchableOpacityProps } from "react-native"
+import { Container, Title } from "./styles"
+
+export type ButtonType = {
+    type?: 'solid' | 'outline'
+    isActive?: boolean
+}
+
+export type ButonProps = TouchableOpacityProps & ButtonType & {
+    title: string
+    icon?: ReactNode
+}
+
+export const Button = ({ type = 'solid', isActive = true, ...props }: ButonProps) => {
+    return (
+        <Container isActive={isActive} type={type} disabled={isActive ? false : true} { ...props }>
+            {props.icon && props.icon}
+
+            <Title type={type} >{props.title}</Title>
+        </Container>
+    )
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,9 @@
+import { Button } from '@components/Button';
 import { Card } from '@components/Card';
 import { Header } from '@components/Header';
+import { Typography } from '@components/Typography';
+import { Plus } from 'phosphor-react-native';
+import { View } from 'react-native';
 import { Container } from './styles';
 
 
@@ -15,6 +19,19 @@ export function HomeScreen() {
           iconPosition='right'
           onPress={() => console.log('Clicou')}
         />
+
+        <View style={{ marginTop: 40}}>
+
+          <Typography fontSize='title_sm' style={{ marginBottom: 8 }}>Refeições</Typography>
+          <Button 
+            title='Nova refeição' 
+            icon={<Plus color='white' size={18} />}
+            type='solid'
+            onPress={() => console.log('Nova refeição')}
+            isActive
+          />
+
+        </View>
     </Container>
   );
 }
