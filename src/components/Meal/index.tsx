78670d17f8a1efdefe31b2ee3f24cb84492a84fe
Diego-Batista <diegobtistadev@gmail.com>
2023-11-11 17:02:26 -0300

    Meal and SectionList Component

---
+++ src/components/Meal/index.tsx
@@ -0,0 +1,31 @@
+import { Typography } from "@components/Typography"
+import { PressableProps } from "react-native"
+import { Container, Divider, Status } from "./styles"
+
+export type MealStatus = {
+    isOnTheDiet: boolean
+}
+
+type MealProps = PressableProps & MealStatus & {
+    id: string
+    hour: string
+    description: string
+}
+
+export const Meal = ({ ...props }: MealProps) => {
+    return (
+        <Container { ...props }>
+            <Typography fontSize="body_xs" family="bold">
+                {props.hour}
+            </Typography>
+
+            <Divider />
+
+            <Typography style={{ flex: 1}} numberOfLines={1}>
+                {props.description}
+            </Typography>
+
+            <Status isOnTheDiet={props.isOnTheDiet}/>
+        </Container>
+    )
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,64 @@
 import { Button } from '@components/Button';
 import { Card } from '@components/Card';
 import { Header } from '@components/Header';
+import { Meal } from '@components/Meal';
 import { Typography } from '@components/Typography';
 import { Plus } from 'phosphor-react-native';
-import { View } from 'react-native';
+import { SectionList, View } from 'react-native';
 import { Container } from './styles';
 
 
 export function HomeScreen() {
+
+  const meals = [
+    {
+      title: '11.11.2023',
+      data: [
+        {
+          id: '01',
+          hour: '20:00',
+          description: 'Whey-protein',
+          isOnTheDiet: true
+        },
+        {
+          id: '02',
+          hour: '22:00',
+          description: 'X-tudo',
+          isOnTheDiet: false
+        },
+        {
+          id: '02',
+          hour: '22:00',
+          description: 'X-tudo',
+          isOnTheDiet: false
+        }
+      ]
+    },
+    {
+      title: '10.11.2023',
+      data: [
+        {
+          id: '03',
+          hour: '20:00',
+          description: 'Whey-protein',
+          isOnTheDiet: true
+        },
+        {
+          id: '04',
+          hour: '22:00',
+          description: 'X-tudo',
+          isOnTheDiet: false
+        },
+        {
+          id: '04',
+          hour: '22:00',
+          description: 'X-tudo',
+          isOnTheDiet: false
+        }
+      ]
+    },
+]
+
   return (
     <Container>
         <Header />
@@ -32,6 +83,47 @@ export function HomeScreen() {
           />
 
         </View>
+
+        <SectionList
+          sections={meals}
+          keyExtractor={(item) => item.id}
+          renderSectionHeader={({section: {title}}) => (
+            <Typography
+              fontSize='title_sm'
+              family='bold'
+              color='gray_1'
+              style={{ marginBottom: 8, marginTop: 32}}
+            >
+              {title}
+            </Typography>
+          )}
+          style={{ marginBottom: 32 }}
+          contentContainerStyle={meals.length === 0 && {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+          showsVerticalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <>
+              <Typography>
+                Não há refeições cadastradas ainda.
+              </Typography>
+              <Typography>
+                Vamos cadastrar refeições hoje?
+              </Typography>
+            </>
+          )}
+          renderItem={({item}) => (
+            <Meal 
+              key={item.id}
+              {...item}
+              onPress={() => console.log('Clicou')}
+            />
+          )}
+        />
+
+        
     </Container>
   );
 }
